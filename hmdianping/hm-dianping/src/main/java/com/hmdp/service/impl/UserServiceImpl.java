169f44fb8304b1f656a3c505b80a3127a3cbd92e
superlsy <12489244+superlsy@user.noreply.gitee.com>
2025-07-06 15:52:06 +0800

    实现手机发送验证码功能

--- hmdianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hmdianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -1,20 +1,45 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpSession;
+
 /**
- * <p>
  * 服务实现类
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
  */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
 
+    /**
+     * 发送验证码
+     *
+     * @param phone
+     * @param session
+     * @return
+     */
+    public Result sendCode(String phone, HttpSession session) {
+        //校验手机号是否正确
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("手机号格式错误");
+        }
+
+        //符合，生成验证码
+        String code = RandomUtil.randomNumbers(6);
+
+        //保存验证码到 session
+        session.getAttribute(code);
+
+        log.info("发送短信成功，生成的验证码为：{}", code);
+
+        return Result.ok();
+    }
 }
