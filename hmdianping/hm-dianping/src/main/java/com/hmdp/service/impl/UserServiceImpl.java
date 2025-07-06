7740fc435d0f6618a00d5dd8c96e4daeeaed13d7
superlsy <12489244+superlsy@user.noreply.gitee.com>
2025-07-06 16:35:52 +0800

    登录功能实现

--- hmdianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ hmdianping/hm-dianping/src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * 服务实现类
@@ -36,10 +39,64 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         String code = RandomUtil.randomNumbers(6);
 
         //保存验证码到 session
-        session.getAttribute(code);
+        session.setAttribute("code", code);
 
         log.info("发送短信成功，生成的验证码为：{}", code);
 
         return Result.ok();
+    }
+
+    /**
+     * 实现登录功能
+     *
+     * @param loginForm
+     * @param session
+     * @return
+     */
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        //校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            //如果不符合，返回错误信息
+            return Result.fail("手机号格式错误");
+        }
+
+        //校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            //不一致，返回错误信息
+            return Result.fail("验证码错误");
+        }
+
+        //用户与验证码一致，跟据手机号查用户
+        User user = query().eq("phone", phone).one();
+
+        //判断用户是否存在
+        if (user == null) {
+            //不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        //保存用户信息到session
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    /**
+     * 创建保存新用户
+     *
+     * @param phone
+     * @return
+     */
+    private User createUserWithPhone(String phone) {
+        //创建新用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+
+        //保存用户
+        save(user);
+        return user;
     }
 }
