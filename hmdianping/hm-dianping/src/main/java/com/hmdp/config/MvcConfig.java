63e5acb81a7d68cb0f95e1d5b885ab34926cc422
superlsy <12489244+superlsy@user.noreply.gitee.com>
2025-07-08 23:06:44 +0800

    登录拦截，隐藏用户敏感信息

---
+++ hmdianping/hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
@@ -0,0 +1,33 @@
+package com.hmdp.config;
+
+import com.hmdp.intercept.LoginIntercept;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 配置拦截器
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 添加拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginIntercept())
+                .excludePathPatterns(
+                        "/user/login",
+                        "/user/code",
+                        "/blog/hot",
+                        "/shop/**",
+                        "/shop-type/**",
+                        "/upload/**",
+                        "/voucher/**"
+
+                );
+    }
+}
--- hmdianping/hm-dianping/src/main/java/com/hmdp/controller/UserController.java
+++ hmdianping/hm-dianping/src/main/java/com/hmdp/controller/UserController.java
@@ -3,9 +3,11 @@ package com.hmdp.controller;
 
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -44,7 +46,7 @@ public class UserController {
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session) {
         //实现登录功能
-        return userService.login(loginForm,session);
+        return userService.login(loginForm, session);
     }
 
     /**
@@ -58,10 +60,15 @@ public class UserController {
         return Result.fail("功能未完成");
     }
 
+    /**
+     * 获取当前登录的用户并返回
+     *
+     * @return
+     */
     @GetMapping("/me")
     public Result me() {
-        // TODO 获取当前登录的用户并返回
-        return Result.fail("功能未完成");
+        UserDTO userDTO = UserHolder.getUser();
+        return Result.ok(userDTO);
     }
 
     @GetMapping("/info/{id}")
